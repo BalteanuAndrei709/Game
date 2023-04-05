b5233df7cdea47b899ab2c1f8e13a89e11d05743
BalteanuAndrei709 <andreibalteanu2@gmail.com>
2023-04-05 15:39:42 +0300

    Menu+ Finish animation + Level 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
     private Animator animator;
 
     private float directionX = 0f;
     [SerializeField]private float moveSpeed = 7f;
     [SerializeField]private float jumpForce = 14;
+    [SerializeField]private LayerMask jumpableGround;
     
     private enum MovementState { idle, running, jumping, falling }
     
@@ -19,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
         directionX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -65,5 +68,10 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.falling;
         }
         animator.SetInteger("state", (int)state);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
